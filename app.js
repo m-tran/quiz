e8b7e378b96cf27cb6f16f03c675439067a8a17f
Michelle Tran <mptran0101@berkeley.edu>
2020-04-25 01:36:42 -0700

    load scores in separate divs for leaderboard

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    
+
     var $quizContainer = $("#quizContainer");
     var nextBtn = $("#nextBtn");
     var submitBtn = $("#submitBtn");
@@ -31,23 +31,24 @@ $(document).ready(function () {
         var highScore = [];
     } else {
         var highScore = JSON.parse(localStorage.getItem('scoreboard'));
+        console.log(highScore);
     }
-    
+
 
     var leaderBoard = {
-        addScore: function(name, score) {
+        addScore: function (name, score) {
             highScore.push([name, score]);
-    
-            highScore.sort(function(a, b) {
+
+            highScore.sort(function (a, b) {
                 return a[1] > b[1];
             });
-    
+
             if (highScore.length > 5) {
                 highScore = highScore.splice(0, 5);
             }
         },
-    
-        saveScores: function() {
+
+        saveScores: function () {
             localStorage.setItem('scoreboard', JSON.stringify(highScore));
         },
     }
@@ -87,42 +88,39 @@ $(document).ready(function () {
         $quizContainer.html(currentQuestion.join("<br>"));
     }
 
-    $('.choices input').on('change', function() {
-        console.log($(`input[name="${questionNumber}"]:checked`, '.choices').val());
-    });
-
     function sum(arr) {
         var i = 0;
-    
+
         for (var index = 0; index < arr.length; index++) {
             i += arr[index];
         }
-    
+
         return i;
     }
 
-    var i=0;
+    var i = 0;
 
     createQuestion(questions[i]);
 
     var scores = [];
     var highScore;
 
-    nextBtn.on("click", function() {
+    nextBtn.on("click", function () {
         // console.log(i);
         answers.push($(`input[name="${questionNumber}"]:checked`, '.choices').val());
         localStorage.setItem("submit", answers);
-        if (i>(questions.length - 2)) {
-            $quizContainer.html(""); 
-            $(".button").html(""); 
+        if (i > (questions.length - 2)) {
+            $quizContainer.html("");
+            $(".button").html("");
             numbers = localStorage.getItem("submit").split(',');
             // console.log(typeof numbers);
-            for (let i=0; i<numbers.length; i++) {
+            for (let i = 0; i < numbers.length; i++) {
                 scores.push(characters[numbers[i]]);
             }
             $quizContainer.append(
-            `<form>
-                <label for="initials">Initials</label><br>
+                `<form>
+                <p>Your final score is: ${sum(scores)}</p>
+                <label for="initials">Please enter initials</label><br>
                 <input type="text" id="initials" name="name">
                 <input id="submitBtn" class="mt-4 button" type="submit" value="Submit">
             </form>`
@@ -133,13 +131,16 @@ $(document).ready(function () {
         }
     });
 
-    $quizContainer.on("click", "input#submitBtn", function(e) {
-        e.preventDefault();
-        console.log($(`input[name="name"]`).val());
-        leaderBoard.addScore($(`input[name="name"]`).val(), sum(scores));
-        leaderBoard.saveScores();
-        $quizContainer.html(""); 
-        $quizContainer.html(highScore); 
-    });
+        $quizContainer.on("click", "input#submitBtn", function (e) {
+            e.preventDefault();
+            leaderBoard.addScore($(`input[name="name"]`).val(), sum(scores));
+            leaderBoard.saveScores();
+            $quizContainer.html("");
+            
+            for (i = 0; i < highScore.length; i++) {
+                $('<div class="row col-sm-8" />').html(highScore[i][0] + " - " + highScore[i][1]).appendTo($quizContainer);
+            }
+            
+        });
 
-});
+    });
